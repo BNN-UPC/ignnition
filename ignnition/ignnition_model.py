e19f7c6f841e40252d78e1908da2d4b0138ff7a6
David Pujol <david.pujolperich@gmail.com>
2021-07-12 20:08:16 +0200

    Added an additional parameter for the predict functionality, to control, if desired, the number of samples to make predictions withs. If no parameter is specified, then it is ignored, and IGNNITION comptues a prediction for each of the provided samples.

--- ignnition/ignnition_model.py
+++ ignnition/ignnition_model.py
@@ -740,7 +740,7 @@ class IgnnitionModel:
                            use_multiprocessing=True,
                            verbose=1)
 
-    def predict(self, prediction_samples=None, verbose=True):
+    def predict(self, prediction_samples=None, verbose=True, num_predictions = None):
         """
         Parameters
         ----------
@@ -786,7 +786,9 @@ class IgnnitionModel:
                 denorm_func = None
 
             # while there are predictions
-            while True:
+            finished = False
+            counter = 0
+            while not finished:
                 pred = self.gnn_model(sample_it.get_next(), training=False)
                 pred = tf.squeeze(pred)
                 output_name = self.model_info.get_output_info()  # for now suppose we only have one output layer_type
@@ -798,6 +800,10 @@ class IgnnitionModel:
                         print_failure('The denormalization function failed')
 
                 all_predictions.append(pred)
+
+                counter += 1
+
+                finished = True if (num_predictions is not None and counter >= num_predictions) else False
 
         except tf.errors.OutOfRangeError:
             pass
