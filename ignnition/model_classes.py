89e6c6ac93e962055588484f97493db2a87648c7
Miquel Ferriol-Galmés <mferriol@ac.upc.edu>
2021-10-20 20:54:31 +0200

    Cleared some useless prints and outputs used to debug information.

--- ignnition/model_classes.py
+++ ignnition/model_classes.py
@@ -138,11 +138,6 @@ class Recurrent_Update_Cell(CustomLayer):
         dst_dim: int
             Dimension of the destination nodes
         """
-        print("dst_dim")
-        print(dst_dim)
-        print("dst_dim")
-        print(dst_dim)
-        #src_input = tf.ensure_shape(src_input, [None, dst_dim])
         new_state, _ = model(src_input, [old_state])
         return new_state
 
